b553cf0cdd3a50c7a1f8da04661409ae00d04da9
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-09-11 18:51:05 +0100

    feat: Scaffolded site

---
+++ src/js/common.js
@@ -0,0 +1,16 @@
+function Compare(left, right) {
+    if (left < right)
+        return -1;
+
+    if (left > right)
+        return 1;
+
+    return 0;
+}
+
+(() => {
+    'use strict'
+
+    feather.replace({ 'aria-hidden': 'true' });
+    
+})();
---
+++ src/js/viewmodel-index.js
@@ -0,0 +1,23 @@
+function ViewModel() {
+    let self = this;
+
+/*$.ajax({
+                    type: 'GET',
+                    url: url,
+                    dataType: 'json',
+                    mimeType: 'application/json',
+                    success: function (data) {
+                        resolve(data);
+                    },
+                    error: function (error) {
+                        reject(error);
+                    }
+                });*/
+                
+    self.Init = function () {
+        
+    };
+}
+
+let vm = new ViewModel();
+vm.Init();
