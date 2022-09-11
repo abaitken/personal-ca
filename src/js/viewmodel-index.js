45fdf3535808b7618d47b6cb29f820fb7b9d42bb
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-09-11 21:12:31 +0100

    feat: Created table and certificate data request

--- src/js/viewmodel-index.js
+++ src/js/viewmodel-index.js
@@ -1,21 +1,30 @@
 function ViewModel() {
     let self = this;
 
-/*$.ajax({
-                    type: 'GET',
-                    url: url,
-                    dataType: 'json',
-                    mimeType: 'application/json',
-                    success: function (data) {
-                        resolve(data);
-                    },
-                    error: function (error) {
-                        reject(error);
-                    }
-                });*/
-                
+    self.certificates = ko.observableArray([]);
+
+    self.fetchCertificates = function() {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                type: 'GET',
+                url: '/api/Certificates',
+                dataType: 'json',
+                mimeType: 'application/json',
+                success: function (data) {
+                    resolve(data);
+                },
+                error: function (error) {
+                    reject(error);
+                }
+            });
+        });
+    };
+
     self.Init = function () {
-        
+        self.fetchCertificates()
+            .then((data) => {
+                self.certificates(data);
+            });
     };
 }
 
